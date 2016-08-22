3d6c048ce689b0fe580145da15539db35c996408
Christian Dondrup <cdondrup@gmail.com>
2016-08-22 16:26:49 +0100

    renaming bridge script

--- rosplan_pnp_bridge/scripts/pnp_bridge.py
+++ rosplan_pnp_bridge/scripts/pnp_bridge.py
@@ -8,13 +8,17 @@ Created on Thu Jul 28 11:06:58 2016
 
 import rospy
 from rosplan_pnp_bridge.gen_bridge import GenBridge
+from rosplan_pnp_bridge.kb_bridge import KnowledgeBaseBridge
+from rosplan_pnp_bridge.rosplan_feedback_server import ROSPlanFeedbackServer
 from rosplan_dispatch_msgs.msg import CompletePlan
 
 
 class ROSPlanBridge(object):
     def __init__(self, name):
         rospy.loginfo("Starting '%s'." % name)
-        self.gen = GenBridge()
+        self.gen = GenBridge() # Used in callback
+        self.fb = ROSPlanFeedbackServer() # Handles the communictaion with the dispatcher just need instance
+        self.kb = KnowledgeBaseBridge() # Does all the KB magic just need instance
         rospy.sleep(.5)
         rospy.Subscriber("/kcl_rosplan/plan", CompletePlan, self.callback)
         rospy.loginfo("Done.")
